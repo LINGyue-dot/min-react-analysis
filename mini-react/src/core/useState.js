eeec617e9acd581eb470ce25d3fb77aad1279516
shunze.chen <qianlonwork@outlook.com>
2024-03-06 00:09:28 +0800

    docs: mini-react 添加额外注释

--- mini-react/src/core/useState.js
+++ mini-react/src/core/useState.js
@@ -1,17 +1,17 @@
 import { commitRender, getCurrentFunctionFiber, getHookIndex } from "./fiber";
 
+// 一个 hook 对应一个 hook 对象，一个 hook 对象存在有环 update 对象
 export function useState(initial) {
   const currentFunctionFiber = getCurrentFunctionFiber();
   const hookIndex = getHookIndex();
 
   // 取旧 fiber 的 hook
-  console.log(currentFunctionFiber);
   const oldHook = currentFunctionFiber?.alternate?.hooks?.[hookIndex];
   const hook = {
     state: oldHook ? oldHook.state : initial,
     queue: [],
   };
-
+  // update 对象 { action,next }
   const actions = oldHook ? oldHook.queue : [];
   actions.forEach((action) => {
     hook.state = action(hook.state);
